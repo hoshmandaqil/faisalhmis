39ee252361cbc819ff5d2211c5edbe3a112debee
Mostafa Rezaie <mostafarezaie.500@gmail.com>
2024-09-09 16:53:36 +0430

    fix: status

--- resources/views/PurchaseOrder/PO.blade.php
+++ resources/views/PurchaseOrder/PO.blade.php
@@ -370,6 +370,7 @@
         <div class="modal-dialog modal-xl" role="document">
             <div class="modal-content">
                 <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Edit Purchase Order</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
