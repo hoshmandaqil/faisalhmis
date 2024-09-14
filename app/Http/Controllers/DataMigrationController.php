049a51d99e5db5f11933c4b3719e04902bbc671b
hoshmandaqil <aqil1995.hoshmand@gmail.com>
2024-09-14 11:13:14 +0430

    shift the incom category

--- app/Http/Controllers/DataMigrationController.php
+++ app/Http/Controllers/DataMigrationController.php
@@ -6,6 +6,7 @@ use App\Models\Expense\ExpenseCategory;
 use App\Models\Expense\ExpenseItem;
 use App\Models\Expense\ExpenseSlip;
 use App\Models\IncomeCategory;
+use App\Models\MiscellaneousIncome;
 use App\Models\PurchaseOrderFile;
 use App\Models\PurchaseOrderItem;
 use App\Models\PurchaseOrder;
@@ -17,7 +18,7 @@ class DataMigrationController extends Controller
     public function index()
     {
         // return 'hello dear';
-        $this->otherIncomeCategory();
+        $this->moveOtherIncome();
     }
 
 
@@ -171,21 +172,25 @@ class DataMigrationController extends Controller
 
     public function moveOtherIncome()
     {
-        $categories = DB::connection('mysql2')->table('expenses_items')->where('application_id', 8)->get();
+        $categories = DB::connection('mysql2')->table('miscellaneous_income')->where('application_id', 8)->get();
         // dd($categories);
         foreach ($categories as $file) {
-            ExpenseItem::insert([
+            MiscellaneousIncome::insert([
                 'id' => $file->id,
-                'slip_id' => $file->slip_id,
-                'expense_description' => $file->expense_description,
+                'slip_no' => $file->slip_no,
+                'paid_by' => $file->paid_by,
+                'paid_to' => $file->paid_to,
+                'date' => $file->date,
+                'income_description' => $file->income_description,
                 'amount' => $file->amount,
-                'quantity' => '',
                 'remarks' => $file->remarks,
+                'category' => $file->category,
+                'cashier' => 8,
                 'created_at' => $file->created_at,
                 'updated_at' => $file->updated_at,
             ]);
         }
 
-        echo "Expense item are shifted successfully.";
+        echo "Other item are shifted successfully.";
     }
 }
