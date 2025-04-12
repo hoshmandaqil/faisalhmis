4e1800b4dab007d3e967937642e1ecd740ff364f
hoshmandaqil <aqil1995.hoshmand@gmail.com>
2025-04-12 10:20:04 +0430

    fix the issu

--- resources/views/ajax/ajax_preview_medicines.blade.php
+++ resources/views/ajax/ajax_preview_medicines.blade.php
@@ -1,103 +1,171 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.master')
 
-<head>
-    <meta charset="utf-8">
-    <title>Patient User</title>
-    <!-- Bootstrap css -->
-    <style media='screen,print'>
-        <?php include public_path('assets/css/bootstrap.min.css'); ?>
-    </style>
-    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" media='all'>
-</head>
+@section('page_title')
+    Patients Invoice
+@endsection
 
-<body>
-    <main>
-        <div class="col-12">
-            <div class="row">
-                <div class="col-12" id="print-preview">
-                    <div>
-                        <div class="table-responsive table-borderless table-light">
-                            <table class="table">
-                                <tr>
-                                    <td class="text-center center" colspan="100%">
-                                        <img src="{{ asset('assets/img/logo/logo.png') }}" alt=""
-                                            style="height: 80px" class="mb-4">
-                                        <h3 class="font-weight-bold">Bayazid Rokhan Hospital</h3>
-                                        <h4>Pharmacy</h4>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <strong>ID:</strong>
-                                        <span>
-                                            {{ $patient->patient_generated_id }}
-                                        </span>
-                                    </td>
-                                    <td><strong>Patient Name:</strong>
-                                        <span>
-                                            {{ $patient->patient_name }}
-                                        </span>
-                                    </td>
-                                </tr>
-                            </table>
-                            <table class="table medicine_table table-bordered border-black"
-                                style="font-size: 15px !important;">
-                                <thead class="table-bordered border-black" style="font-weight:bold; ">
-                                    <th>ITEM</th>
-                                    <th>RATE</th>
-                                    <th>QTY</th>
-                                    <th>AMOUNT</th>
-                                </thead>
-                                <tbody class="table-bordered border-black">
-                                    <?php $grandTotal = 0; ?>
-                                    @foreach ($soldMedicines as $medicine)
-                                        <tr>
-                                            <td>{{ ucfirst($medicine->medicine->medicine_name) }}</td>
-                                            <td>{{ $medicine->quantity }}</td>
-                                            <td>{{ round($medicine->unit_price) }}</td>
-                                            <td>{{ round($medicine->unit_price * $medicine->quantity) }}</td>
-                                            <?php $grandTotal += $medicine->unit_price * $medicine->quantity; ?>
-                                        </tr>
-                                    @endforeach
-                                    <tr>
-                                        <td style="border-top: 1px solid lightgray; font-weight:bold;" colspan="100%">
-                                            <b>Total: {{ round($grandTotal) }} AFN</b>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                            <div class="px-2 d-flex justify-content-between">
-                                <p style="font-size:14px"><strong>By:</strong> {{ $patient->createdBy->name }}</p>
-                                <p style="font-size:14px"><strong>Address:</strong> Karte-Naw, Nanwayi Station</p>
-                            </div>
-                            <div class="px-2 d-flex justify-content-between">
-                                <p style="font-size:14px"><strong>Date:</strong> {{ $patient->created_at }}</p>
-                                <p style="font-size:14px"><strong>Phone No:</strong>078 700 04 44</p>
+@section('page-action')
+@endsection
+
+@section('styles')
+@endsection
+
+@section('content')
+
+<div class="submit-section d-flex justify-content-end px-4">
+    <button class="btn btn-dark btn-sm hidden-print d-print-none" type="button"
+        onclick="printSpecificDiv('print-preview')">
+        Print
+        <i class="icon icon-print"></i>
+    </button>
+</div>
+
+<div id="print-preview">
+    <div class="row gutters">
+        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+            <div class="row gutters">
+                <div class="col-6 offset-3 text-center">
+                    <p class="title" style="font-size: 1.3rem">Ministry of Health</p>
+                    <p class="title" style="font-size: 1.2rem">Bayazid Rokhan Hospital</p>
+                    <p class="title" style="font-size: 1.1rem">Finance Department</p>
+                    <p class="title" style="font-size: 1rem">Patient Invoice</p>
+                </div>
+            </div>
+
+            <div class="invoice-container">
+                <hr>
+                <div class="invoice-body col-12 offset-3">
+
+                    <div class="row gutters">
+                        <div class="col-lg-6 col-md-6 col-sm-6">
+                            <div class="row gutters">
+                                <div class="col-lg-6 col-md-6 col-sm-6">
+                                    <div class="row ml-2">
+                                        <h6>Patient Name:</h6>
+                                        <h5 class="ml-1">{{ ucfirst($patient->patient_name) }}</h5>
+                                    </div>
+                                    <div class="row ml-2">
+                                        <h6>Patient ID:</h6>
+                                        <h5 class="ml-1">{{ $patient->patient_generated_id }}</h5>
+                                    </div>
+                                    <div class="row ml-2">
+                                        <h6>Doctor:</h6>
+                                        <h5 class="ml-1">{{ $patient->doctor->name }}</h5>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
+
+                    <div class="invoice-payment">
+                        <div class="row gutters">
+                            <div class="col-lg-6 col-md-6 col-sm-12 order-last">
+                                <table class="table no-border m-0">
+                                    <tbody>
+                                        <tr>
+                                            <th></th>
+                                            <th>Original Price</th>
+                                            <th>Discount</th>
+                                            <th>Payable</th>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <p>
+                                                    OPD Fee<br>
+                                                    IPD Fee<br>
+                                                    Pharmacy Charges<br><br>
+                                                    <b>Diagnose Fee</b><br>
+                                                    @foreach ($patient->laboratoryTests as $labTest)
+                                                        {{ $labTest->testName->dep_name }} <br>
+                                                    @endforeach
+                                                </p>
+                                                <h5 class="text-danger"><strong>Grand Total</strong></h5>
+                                            </td>
+                                            <?php
+                                                $totalIPD = 0;
+                                                $totalIPD_discount = 0;
+                                                $totalPharmacy = 0;
+                                                $totalLab = 0;
+                                                $totalLabDiscount = 0;
+
+                                                if ($patient->ipd != NULL) {
+                                                    $totalPrice = 0;
+                                                    $totalDiscount = 0;
+                                                    if ($patient->ipd->discharge_date != NULL) {
+                                                        $register_date = \Carbon\Carbon::parse(date('Y-m-d', strtotime($patient->ipd->created_at)));
+                                                        $discharge_date = $patient->ipd->discharge_date;
+                                                        $ipdDays = $register_date->diffInDays($discharge_date);
+
+                                                        for ($i = 1; $i <= $ipdDays; $i++) {
+                                                            $totalPrice += $patient->ipd->price;
+                                                            $discountForTest = ($patient->ipd->discount * $patient->ipd->price) / 100;
+                                                            $totalDiscount += $discountForTest;
+                                                        }
+                                                    }
+                                                    $totalIPD += $totalPrice - $totalDiscount;
+                                                    $totalIPD_discount += $totalDiscount;
+                                                }
+
+                                                if ($patient->pharmacyMedicines != NULL) {
+                                                    foreach ($patient->pharmacyMedicines as $medicine) {
+                                                        $totalPharmacy += $medicine->quantity * $medicine->unit_price;
+                                                    }
+                                                }
+                                            ?>
+                                            <td>
+                                                <p>
+                                                    {{ number_format($patient->OPD_fee) }} AF<br>
+                                                    {{ number_format($totalIPD + $totalIPD_discount) }} AF<br>
+                                                    {{ number_format($totalPharmacy) }} AF<br><br><br>
+                                                    @foreach ($patient->laboratoryTests as $labTest)
+                                                        {{ number_format($labTest->price) }} <br>
+                                                        <?php $totalLab += $labTest->price; ?>
+                                                    @endforeach
+                                                </p>
+                                                <h5 class="text-danger"><strong>{{ number_format($patient->OPD_fee + $totalIPD + $totalIPD_discount + $totalPharmacy + $totalLab) }}</strong></h5>
+                                            </td>
+                                            <td>
+                                                <p>
+                                                    0 AF<br>
+                                                    {{ number_format($totalIPD_discount) }} AF<br>
+                                                    0 AF<br><br><br>
+                                                    @foreach ($patient->laboratoryTests as $labTest)
+                                                        {{ number_format(($labTest->discount * $labTest->price) / 100) }} <br>
+                                                        <?php $totalLabDiscount += ($labTest->discount * $labTest->price / 100); ?>
+                                                    @endforeach
+                                                </p>
+                                                <h5 class="text-danger"><strong>{{ number_format($totalIPD_discount + $totalLabDiscount) }}</strong></h5>
+                                            </td>
+                                            <td>
+                                                <p>
+                                                    {{ number_format($patient->OPD_fee) }} AF<br>
+                                                    {{ number_format($totalIPD) }} AF<br>
+                                                    {{ number_format($totalPharmacy) }} AF<br><br><br>
+                                                    @foreach ($patient->laboratoryTests as $labTest)
+                                                        {{ number_format($labTest->price - ($labTest->discount * $labTest->price) / 100) }} <br>
+                                                    @endforeach
+                                                </p>
+                                                <h5 class="text-danger"><strong>{{ number_format($patient->OPD_fee + $totalIPD + $totalPharmacy + $totalLab - $totalLabDiscount) }}</strong></h5>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div><!-- invoice-payment -->
+                </div><!-- invoice-body -->
+            </div><!-- invoice-container -->
         </div>
-    </main>
-
-    <div class="submit-section d-flex justify-content-end px-4">
-        <button class="btn btn-dark btn-sm hidden-print d-print-none" type="button"
-            onclick="printSpecificDiv('print-preview')">
-            Print
-            <i class="icon icon-print"></i>
-        </button>
     </div>
-    <br class="d-print-none">
-</body>
+</div><!-- end #print-preview -->
 
-</html>
+@endsection
 
+@section('scripts')
 <script>
     function printSpecificDiv(divId) {
         var printContent = document.getElementById(divId).innerHTML;
-        var printWindow = window.open('', '_blank');
+        var printWindow = window.open();
 
         printWindow.document.open();
         printWindow.document.write(`
@@ -138,7 +206,7 @@
             </body>
             </html>
         `);
-        
         printWindow.document.close();
     }
 </script>
+@endsection
