2a16f94af4d86acc612c530e8726e6e7d2eaabf7
hoshmandaqil <aqil1995.hoshmand@gmail.com>
2025-06-24 14:10:50 +0430

    fix the issue

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -24,14 +24,14 @@ class PatientController extends Controller
     {
         $patients = Patient::latest()->with('doctor', 'createdBy')->paginate(30);
         //$doctors = User::where('type', 3)->latest()->pluck('name', 'id')->all();
-        $targetIds = [25, 23, 24, 64,91];
+        $targetIds = [25, 23, 24, 64];
         $doctors = User::where('type', 3)
             ->where('status', 1)
             ->whereHas('employee', function ($query) {
             $query->where('status', 1);
             })
             ->when(!in_array(auth()->user()->id, $targetIds), function ($query) {
-            $query->orWhereIn('id', [28, 47, 77]);
+            $query->orWhereIn('id', [28, 47, 77,91]);
             })
             ->latest()
             ->get();
